95313304a1022c78584dadfee093438c9f35a494
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-11-19 06:24:20 +0000

    cleaned merge finale

--- populations.py
+++ populations.py
@@ -1,9 +1,12 @@
+from shlex import join
 import pandas as pd
+import numpy as np
 
 "data_2006_2010"
 a = pd.read_csv("/workspaces/opioid-2023-ss1486-us26-avb26-dj216/data_pop/Population_2006_2010.csv", encoding = 'latin-1')
-"data_2010_2015"
 a = a[['STNAME', 'CTYNAME', 'POPESTIMATE2006', 'POPESTIMATE2007', 'POPESTIMATE2008', 'POPESTIMATE2009']]
+
+"data_2010_2015"
 b = pd.read_csv("/workspaces/opioid-2023-ss1486-us26-avb26-dj216/data_pop/Population_2010_2015.csv", encoding = 'latin-1')
 b = b[['STNAME', 'CTYNAME', "POPESTIMATE2010",'POPESTIMATE2011', 'POPESTIMATE2012', 'POPESTIMATE2013', 'POPESTIMATE2014', 'POPESTIMATE2015']]
 
@@ -16,7 +19,7 @@ b = b[['STNAME', 'CTYNAME', "POPESTIMATE2010",'POPESTIMATE2011', 'POPESTIMATE201
 "This effectively gives you the county names in a that are not in b"
 
 # print (a[~a['CTYNAME'].isin(b['CTYNAME'])])
-print(a.loc[[88,94,1161,2963]])
+# print(a.loc[[88,94,1161,2963]])
 # 88: Alaska	North Slope Borough
 # 94: Alaska	Southeast Fairbanks Census Area
 # 1161: Louisiana	Lafayette Parish
@@ -26,7 +29,20 @@ print(a.loc[[88,94,1161,2963]])
 
 " This effectively gives you the county names in b that are not in a"
 # print (b[~b['CTYNAME'].isin(a['CTYNAME'])])
-print(b.loc[[83,89,1161,2454]])
+# print(b.loc[[83,89,1161,2454]])
+a['CTYNAME'] = np.where((a['STNAME'] == 'South Dakota') & (a['CTYNAME'] == 'Shannon County') , "Oglala Lakota County", a['CTYNAME'])
+#a['CTYNAME'] = a['CTYNAME'].replace({'Shannon County' : 'Oglala Lakota County'})
+a['CTYNAME'] = a['CTYNAME'].replace({'La Salle Parish': 'LaSalle Parish' })
+a['CTYNAME'] = a['CTYNAME'].replace({ 'Petersburg Census Area' : 'Petersburg Borough'})
+a['CTYNAME'] = a['CTYNAME'].replace({'Wade Hampton Census Area': 'Kusilvak Census Area'})
+
+
+#drop Bedford city from a
+
+a.drop(a[a['CTYNAME'] == 'Bedford city'].index, inplace=True)
+
+
+
 # 83: Alaska	Ketchikan Gateway Borough
 # 89: Alaska	North Slope Borough
 # 1161: Louisiana	Lafayette Parish
@@ -36,12 +52,26 @@ print(b.loc[[83,89,1161,2454]])
 
 
 
-# print(a.shape)
-# print(b.shape)
-x = pd.merge(a, b, on=['STNAME', 'CTYNAME'])
-x.to_csv("/workspaces/opioid-2023-ss1486-us26-avb26-dj216/data_pop/pop_merge.csv", index=False)
+print(a.shape)
+print(b.shape)
+x = pd.merge(a, b, on=['STNAME', 'CTYNAME'], how='inner')
+
+x.drop_duplicates(keep=False, inplace=True) 
+
+
+# x.to_csv("/workspaces/opioid-2023-ss1486-us26-avb26-dj216/data_pop/pop_merge.csv", index=False)
 # print(x.head())
 print(x.shape)
 
+id_vars = ["STNAME", "CTYNAME"]
+df = x.melt(id_vars =id_vars)
+df = df.rename(columns = {"variable": "Year", "value": "Population"})
 
 
+
+# print (x[~x['CTYNAME'].isin(b['CTYNAME'])])
+# print (x[~x['CTYNAME'].isin(a['CTYNAME'])])
+
+df["Year"] = df["Year"].str.replace("POPESTIMATE", "")
+print(df.head())
+df.to_csv("/workspaces/opioid-2023-ss1486-us26-avb26-dj216/data_pop/pop_merge.csv", index=False)
